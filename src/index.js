b88ce8bde58a8d88dbc1c7f1139e49c673cac842
Raquel Canhoto <raquelcanhoto@Raquels-MBP.lan>
2024-01-17 23:24:48 +0000

    JS automatically update time/date

---
+++ src/index.js
@@ -0,0 +1,22 @@
+function timeUpdade(){
+//Lisbon
+let lisbonElement = document.querySelector("#lisbon");
+let lisbonDateElement = lisbonElement.querySelector(".date");
+let lisbonTimeElement = lisbonElement.querySelector(".time");
+let lisbonTime = moment().tz("Europe/Lisbon");
+
+lisbonDateElement.innerHTML = lisbonTime.format("MMMM Do YYYY");
+lisbonTimeElement.innerHTML = lisbonTime.format("hh:mm:ss [<small>]A[</small>]");
+
+//Switzerland
+let zurichElement = document.querySelector("#zurich");
+let zurichDateElement = zurichElement.querySelector(".date");
+let zurichTimeElement = zurichElement.querySelector(".time");
+let zurichTime = moment().tz("Europe/Zurich");
+
+zurichDateElement.innerHTML = zurichTime.format("MMMM Do YYYY");
+zurichTimeElement.innerHTML = zurichTime.format("hh:mm:ss [<small>]A[</small>]");
+};
+
+timeUpdade();
+setInterval(timeUpdade, 1000);
